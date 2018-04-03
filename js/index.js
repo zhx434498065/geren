df6fd9b2f4d7e29a0022503acc65b2a388240881
zhx434498065 <434498065@qq.com>
2018-04-03 08:49:27 +0800

    1

---
+++ js/index.js
@@ -0,0 +1,9 @@
+var dh=document.querySelectorAll(".daohang");
+var dhyc=document.querySelectorAll(".daohang_yincang");
+// console.log(dh);
+// console.log(dhyc);
+for(let i=0;i<dh.length;i++){
+    dh.onclick=function () {
+        dhcy[i]=stype.opacity=1;
+    }
+}
